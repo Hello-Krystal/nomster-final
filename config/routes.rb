8532d79836b45851ca7d53172b5feda69966c5c8
Krystal <krystal.raysor@gmail.com>
2019-10-11 11:17:16 +0000

    Lesson 33: Form Submission

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'places#index' #makes places controller the root/index page
-  resources :places #allows us to create a new place
+  resources :places do #allows us to create a new place
+    resources :comments, only: :create
+  end
 end
