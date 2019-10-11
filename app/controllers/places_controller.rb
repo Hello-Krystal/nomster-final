2743116c9933bba5e4db93f802beabe7d8a8e110
Krystal <krystal.raysor@gmail.com>
2019-10-11 07:15:04 +0000

    Lesson 25: Edit user permissions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 # lesson 4: build a new controller for the root page to live and have the root go to the index action
 class PlacesController < ApplicationController
   #Before filters are code that executes before the code in our controller runs
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
     @places = Place.all
@@ -24,10 +24,18 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+    end
   end
 
   def update
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+
     @place.update_attributes(place_params)
     redirect_to root_path
   end
