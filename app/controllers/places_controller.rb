6fdaf59503b1432f720a9cad68f996c97a754191
Krystal <krystal.raysor@gmail.com>
2019-10-11 08:12:47 +0000

    Lesson 27-28 Validating User Input, completed Challenge of adding additional validations

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 # lesson 4: build a new controller for the root page to live and have the root go to the index action
 class PlacesController < ApplicationController
   #Before filters are code that executes before the code in our controller runs
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.all
@@ -13,9 +13,13 @@ class PlacesController < ApplicationController
     @place = Place.new
   end
 
-   def create
-    current_user.places.create(place_params) #creates place connected with user and sends def place_params values to the database
-    redirect_to root_path #the page we want to send the user to
+  def create #creates place connected with user and sends def place_params values to the database
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
  
   def show
@@ -26,22 +30,31 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
 
     if @place.user != current_user
-    return render plain: 'Not Allowed', status: :forbidden
+      return render plain: 'Not Allowed', status: :forbidden
     end
   end
 
   def update
-    @place = Place.find(params[:id])
+  @place = Place.find(params[:id])
+
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end
 
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+
     @place.destroy
     redirect_to root_path
   end
